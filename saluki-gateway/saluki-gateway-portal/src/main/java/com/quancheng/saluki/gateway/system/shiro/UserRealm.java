9abd7a3cdbe4e07ce3fc2f39a427e287522d5eee
shiming.liu <shiming.liu@dianrong.com>
2018-01-10 10:42:28 +0800

    update

--- saluki-gateway/saluki-gateway-portal/src/main/java/com/quancheng/saluki/gateway/system/shiro/UserRealm.java
+++ saluki-gateway/saluki-gateway-portal/src/main/java/com/quancheng/saluki/gateway/system/shiro/UserRealm.java
@@ -23,48 +23,41 @@ import com.quancheng.saluki.gateway.system.service.MenuService;
 import com.quancheng.saluki.gateway.utils.ShiroUtils;
 
 public class UserRealm extends AuthorizingRealm {
-/*	@Autowired
-	UserDao userMapper;
-	@Autowired
-	MenuService menuService;*/
 
-	@Override
-	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection arg0) {
-		Long userId = ShiroUtils.getUserId();
-		MenuService menuService = ApplicationContextRegister.getBean(MenuService.class);
-		Set<String> perms = menuService.listPerms(userId);
-		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-		info.setStringPermissions(perms);
-		return info;
-	}
+  @Override
+  protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection arg0) {
+    Long userId = ShiroUtils.getUserId();
+    MenuService menuService = ApplicationContextRegister.getBean(MenuService.class);
+    Set<String> perms = menuService.listPerms(userId);
+    SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+    info.setStringPermissions(perms);
+    return info;
+  }
 
-	@Override
-	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-		String username = (String) token.getPrincipal();
-		Map<String, Object> map = new HashMap<>(16);
-		map.put("username", username);
-		String password = new String((char[]) token.getCredentials());
-
-		UserDao userMapper = ApplicationContextRegister.getBean(UserDao.class);
-		// 查询用户信息
-		UserDO user = userMapper.list(map).get(0);
-
-		// 账号不存在
-		if (user == null) {
-			throw new UnknownAccountException("账号或密码不正确");
-		}
-
-		// 密码错误
-		if (!password.equals(user.getPassword())) {
-			throw new IncorrectCredentialsException("账号或密码不正确");
-		}
-
-		// 账号锁定
-		if (user.getStatus() == 0) {
-			throw new LockedAccountException("账号已被锁定,请联系管理员");
-		}
-		SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(user, password, getName());
-		return info;
-	}
+  @Override
+  protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token)
+      throws AuthenticationException {
+    String username = (String) token.getPrincipal();
+    Map<String, Object> map = new HashMap<>(16);
+    map.put("username", username);
+    String password = new String((char[]) token.getCredentials());
+    UserDao userMapper = ApplicationContextRegister.getBean(UserDao.class);
+    // 查询用户信息
+    UserDO user = userMapper.list(map).get(0);
+    // 账号不存在
+    if (user == null) {
+      throw new UnknownAccountException("账号或密码不正确");
+    }
+    // 密码错误
+    if (!password.equals(user.getPassword())) {
+      throw new IncorrectCredentialsException("账号或密码不正确");
+    }
+    // 账号锁定
+    if (user.getStatus() == 0) {
+      throw new LockedAccountException("账号已被锁定,请联系管理员");
+    }
+    SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(user, password, getName());
+    return info;
+  }
 
 }

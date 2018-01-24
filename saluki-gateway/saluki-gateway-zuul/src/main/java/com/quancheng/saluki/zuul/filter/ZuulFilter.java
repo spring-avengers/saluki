c48ab5e1fa0deee5f65136b61538f71e10634e42
shiming.liu <shiming.liu@dianrong.com>
2018-01-24 11:13:04 +0800

    up

--- saluki-gateway/saluki-gateway-zuul/src/main/java/com/quancheng/saluki/zuul/filter/ZuulFilter.java
+++ saluki-gateway/saluki-gateway-zuul/src/main/java/com/quancheng/saluki/zuul/filter/ZuulFilter.java
@@ -4,9 +4,9 @@ package com.quancheng.saluki.zuul.filter;
  */
 public interface ZuulFilter extends Ordered, Comparable<ZuulFilter> {
 
-    /**
-     * @return
-     */
-    String type();
+  /**
+   * @return
+   */
+  String type();
 
 }

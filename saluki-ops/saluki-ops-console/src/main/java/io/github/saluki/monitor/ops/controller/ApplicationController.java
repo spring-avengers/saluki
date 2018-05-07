0f53028021679b56ce31f44d2875d563f0b9a2be
shiming.liu <shiming.liu@dianrong.com>
2018-05-07 17:38:47 +0800

    support maven center

---
+++ saluki-ops/saluki-ops-console/src/main/java/io/github/saluki/monitor/ops/controller/ApplicationController.java
@@ -0,0 +1,39 @@
+package io.github.saluki.monitor.ops.controller;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.saluki.monitor.domain.Application;
+import io.github.saluki.monitor.domain.ApplicationDependcy;
+import io.github.saluki.monitor.ops.service.ApplicationDependcyService;
+import io.github.saluki.monitor.ops.service.ConsulRegistryService;
+
+@RestController
+@RequestMapping(value = "/api/application")
+public class ApplicationController {
+
+  private static final Logger log = Logger.getLogger(ApplicationController.class);
+
+  @Autowired
+  private ConsulRegistryService registrySerivce;
+
+  @Autowired
+  private ApplicationDependcyService appDependcyService;
+
+  @RequestMapping(value = "list", method = RequestMethod.GET)
+  public List<Application> listAllApps() {
+    log.info("Return all application from registry");
+    return registrySerivce.getAllApplication();
+  }
+
+  @RequestMapping(value = "dependcy", method = RequestMethod.GET)
+  public List<ApplicationDependcy> listDependcyApps() {
+    return appDependcyService.queryApplicationDependcy();
+  }
+
+}
---
+++ saluki-ops/saluki-ops-console/src/main/java/io/github/saluki/monitor/ops/repository/InvokeMapper.java
@@ -0,0 +1,22 @@
+package io.github.saluki.monitor.ops.repository;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import io.github.saluki.monitor.domain.GrpcInvoke;
+import io.github.saluki.monitor.domain.GrpcnvokeStatistics;
+
+@Mapper
+public interface InvokeMapper {
+
+    void addInvoke(List<GrpcInvoke> invokes);
+
+    List<GrpcnvokeStatistics> queryData(Map<String, String> paramter);
+
+    List<Map<String, String>> queryConsumer();
+
+    List<Map<String, String>> queryProvider(Map<String, String> paramter);
+
+}

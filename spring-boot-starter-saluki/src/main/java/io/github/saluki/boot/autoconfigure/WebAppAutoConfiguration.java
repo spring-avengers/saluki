da70c3de3d49d6493d1a0464a8ec903ad6b48613
shiming.liu <shiming.liu@bkjk.com>
2019-05-02 17:07:41 +0800

    upgrade spring boot verson

--- spring-boot-starter-saluki/src/main/java/io/github/saluki/boot/autoconfigure/WebAppAutoConfiguration.java
+++ spring-boot-starter-saluki/src/main/java/io/github/saluki/boot/autoconfigure/WebAppAutoConfiguration.java
@@ -1,14 +1,12 @@
 /*
- * Copyright (c) 2016, Quancheng-ec.com All right reserved. This software is the
- * confidential and proprietary information of Quancheng-ec.com ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Quancheng-ec.com.
+ * Copyright (c) 2016, Quancheng-ec.com All right reserved. This software is the confidential and proprietary
+ * information of Quancheng-ec.com ("Confidential Information"). You shall not disclose such Confidential Information
+ * and shall use it only in accordance with the terms of the license agreement you entered into with Quancheng-ec.com.
  */
 package io.github.saluki.boot.autoconfigure;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,17 +17,17 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnBean(value = GrpcProperties.class)
-public class WebAppAutoConfiguration implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
+public class WebAppAutoConfiguration implements ApplicationListener<WebServerInitializedEvent> {
 
     private final GrpcProperties grpcProperties;
 
-    public WebAppAutoConfiguration(GrpcProperties thrallProperties){
+    public WebAppAutoConfiguration(GrpcProperties thrallProperties) {
         this.grpcProperties = thrallProperties;
     }
 
     @Override
-    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
-        int httpPort = event.getEmbeddedServletContainer().getPort();
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        int httpPort = event.getWebServer().getPort();
         if (grpcProperties.getRegistryHttpPort() == 0) {
             if (httpPort != 0) {
                 grpcProperties.setRegistryHttpPort(httpPort);
